d3e73ebb1a1a3b6193480a59e8bd214407fa879d
Sandro Santana <osandro@protonmail.com.br>
2022-12-07 14:21:07 -0300

    enum

--- Model/profissional.py
+++ Model/profissional.py
@@ -1,8 +1,30 @@
 from Model.pessoa import Pessoa
 
 
+class Nivel(Enum):
+    Junior = 1
+    Pleno = 2
+    Senior = 3
+
+
 class Profissional(Pessoa):
     def __init__(self, nome, cod, cursou, nivel):
         super().__init__(nome, cod)
         self.__cursou = cursou
         self.__nivel = nivel
+
+    @property
+    def cursou(self):
+        return sel.__cursou
+
+    @property
+    def nivel(self):
+        return self.__nivel
+
+    @cursou.setter
+    def cursou(self, curso):
+        self.__cursou = curso
+
+    @nivel.setter
+    def nivel(self, nivel: Nivel):
+        sel.__nivel = nivel
