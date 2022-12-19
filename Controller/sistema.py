47a0d73b2f02c23ddd6ef799340ee25e274688e1
Sandro Santana <osandro@protonmail.com.br>
2022-12-19 14:46:19 -0300

    menu tela sistema

--- Controller/sistema.py
+++ Controller/sistema.py
@@ -1,1 +1,9 @@
 from controlador_curso import ControladorCurso
+
+
+class Sistema:
+    def __init__(self):
+        self.__controlador_curso = ControladorCurso()
+
+    def menu(self):
+        while True:
---
+++ View/tela_sistema.py
@@ -0,0 +1,29 @@
+import PySimpleGUI as sg
+
+
+class TelaSistema:
+    def __init__(self):
+        self.__window = None
+        self.__layout = None
+
+    def close(self):
+        self.__window.Close()
+
+    def open(self):
+        button, values = self.__window.Read()
+        return button, values
+
+    def menu(self):
+        layout = [
+
+            [sg.Text('Menu', font='Lucida')],
+            [sg.Button('Cursos', key=1),
+             sg.Button('Profissionais', key=2),
+             sg.Button('Universitários', key=3)],
+            [sg.Button('Voltar', key=0)]
+
+                 ]
+        self.__window = sg.Window('Menu Principal').layout(layout)
+        op = self.open()
+        self.close()
+        return op[0]
